83325cbdd8cb3bf32814df3aa48d1e28e2d67167
Gabriel Francis Perez <gabriel.francis.perez@gmail.com>
2025-06-28 19:53:16 +0800

    Set up and test API call
    Do test and initial API call to see if it's callable

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 import express from "express";
+import axios from "axios";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
+const API_URL = "https://v2.jokeapi.dev/";
+
+app.get("/", async (req, res) => {
+    try {
+        const result = await axios.get(API_URL + "/joke/Any");
+        const data = JSON.stringify(result.data)
+        console.log("result:", data);
+        res.render("index.ejs", { content: data });
+    } catch (error) {
+        console.error("Failed to make request:", error.message);
+    }
 });
+
+// app.get("/getJoke", async (req, res) => {
+//     try {
+//         const result = await axios.get(API_URL + "/joke/Any");
+//         console.log("result:", result);
+//         res.render("index.ejs", { content: result });
+//     } catch (error) {
+//         console.error("Failed to make request:", error.message);
+//     }
+// });
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}.`);
