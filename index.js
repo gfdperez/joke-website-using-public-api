7e0aa40131edb3e851463517864ccbadfb6570eb
Gabriel Francis Perez <gabriel.francis.perez@gmail.com>
2025-07-13 18:23:43 +0800

    Initial Front-End (Single Joke)
    Add initial design for single joke type

--- index.js
+++ index.js
@@ -11,28 +11,37 @@ const API_URL = "https://v2.jokeapi.dev/";
 
 app.use("/public", express.static(__dirname + "/public"));
 app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
-app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+let joke = "";
+let isSingleJoke = true;
 app.get("/", async (req, res) => {
+    const viewsData = {
+        isSingleJoke: isSingleJoke,
+        joke: joke
+    }
+
+    res.render(__dirname + "/views/index.ejs", viewsData);
+});
+
+app.post("/setJokeType", async (req, res) => {
+    isSingleJoke = req.body.type === "single";
+    res.redirect("/");
+});
+
+app.post("/getSingleJoke", async (req, res) => {
     try {
-        const result = await axios.get(API_URL + "/joke/Any");
-        const data = JSON.stringify(result.data)
-        console.log("result:", data);
-        res.render("index.ejs", { content: data });
+        console.log("req.body:", req.body);
+        isSingleJoke = true;
+        const result = await axios.get(API_URL + "/joke/Any?type=single");
+        joke = result.data.joke;
+        console.log("result:", joke);
+
+        res.redirect("/");
     } catch (error) {
         console.error("Failed to make request:", error.message);
     }
 });
-
-// app.get("/getJoke", async (req, res) => {
-//     try {
-//         const result = await axios.get(API_URL + "/joke/Any");
-//         console.log("result:", result);
-//         res.render("index.ejs", { content: result });
-//     } catch (error) {
-//         console.error("Failed to make request:", error.message);
-//     }
-// });
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}.`);
